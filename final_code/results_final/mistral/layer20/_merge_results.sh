e1f9dc523522e34d3eede397e247c716db5a8b1d
Max Feser <s37mfese@uni-bonn.de>
2026-03-18 15:12:11 +0100

    preemptive merged_results for layer66

---
+++ final_code/results_final/mistral/layer20/_merge_results.sh
@@ -0,0 +1,4 @@
+python \
+ /home/s37mfese/MA-NF_4335-Lab_AI_Alignment/src/final_code/merge_method_tables.py \
+ /home/s37mfese/MA-NF_4335-Lab_AI_Alignment/src/final_code/results_final/mistral/layer20 \
+ --all-tables
